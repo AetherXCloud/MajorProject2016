d027d9390b5de517924d14cbdb74975d9f50ba3b
Jordan <epicpotatoe3@gmail.com>
2016-04-29 08:27:04 +1000

    Updated main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,61 @@
+var bound = false;
 $(function(){
-
-});
-$(window).scroll(function(){
-	var wScroll = $(this).scrollTop();
-	var viewportHeight = $( window ).height();
-	var headerHeight= $('header').height();
-	if(wScroll > 0){
-		$('header').css({
-			'background-color' : 'rgba(0, 0, 0, 0.5)'
-		});
+	if($( window ).width() < 800){
+		bindApprenticeships();
 	} else {
-		$('header').css({
-			'background-color' : 'rgba(0, 0, 0, 0)'
+		$('section#apprenticeship-section .inner .column .title').next().css('display', 'block');
+		$('section#apprenticeship-section .inner .column .title').each(function(){
+			$(this).children('p').children('strong').text($(this).text().slice(0,-2));
 		});
 	}
 });
+$( window ).resize(function() {
+	if($( window ).width() < 800) {
+		bindApprenticeships(true);
+	} else {
+		unbindApprenticeships();
+	}
+});
+
+// $(window).scroll(function(){
+// 	var wScroll = $(this).scrollTop();
+// 	var viewportHeight = $( window ).height();
+// 	var headerHeight= $('header').height();
+// });
+
+
+function bindApprenticeships(addDropdown = false) {
+	if(!bound){
+		bound = true;
+		if (addDropdown) {
+			$('section#apprenticeship-section .inner .column .title').each(function(){
+				$(this).children('p').children('strong').text($(this).text() + " ▼");
+			});
+		}
+
+		$('section#apprenticeship-section .inner .column .title').on("click", function(){
+			var oldtext = $(this).children('p').children('strong').text();
+			var newtext;
+			if (oldtext.indexOf("▼") >= 0) {
+				newtext = oldtext.slice(0,-1) + "▲";
+			} else {
+				newtext = oldtext.slice(0,-1) + "▼";
+			}
+			$(this).children('p').children('strong').text(newtext);
+			$(this).next().slideToggle("normal");
+		});
+
+		$('section#apprenticeship-section .inner .column .title').next().css('display', 'none');
+	}
+}
+
+function unbindApprenticeships() {
+	if (bound){
+		bound = false;
+		$('section#apprenticeship-section .inner .column .title').off();
+		$('section#apprenticeship-section .inner .column .title').next().css('display', 'block');
+		$('section#apprenticeship-section .inner .column .title').each(function(){
+			$(this).children('p').children('strong').text($(this).text().slice(0,-2));
+		});
+	}
+}
